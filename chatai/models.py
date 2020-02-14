76ad80036b57ebf9b22dabea1b6ac5cfbf38ee55
ll6526656 <ll6526656@qq.com>
2020-02-14 21:48:19 +0800

    first commit

---
+++ chatai/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Hit(models.Model):
+	security_code = models.IntegerField()
+	worker1_name = models.CharField(max_length=50)
+	worker2_name = models.CharField(max_length=50)
+	current_conv = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+	def __str__(self):
+		return ""
+
+class Uterrance(models.Model):
+	text = models.CharField(max_length=200)
+	speaker_idx = models.IntegerField()
+	self_eval = models.IntegerField(default=5)
+	def __str__(self):
+		return ""
+
+class Conversation(model.Model):
+	hit = models.ForeignKey(Hit,on_delete=models.CASCADE)
+	utterances = models.OneToOneField(Hit, on_delete=models.CASCADE, primary_key=True)
+	def __str__(self):
+		return ""
+	
---
+++ chatai/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+app_name = "chatai"
+urlpatterns = [
+	path('',views.IndexView.as_view(), name="index"),
+]
